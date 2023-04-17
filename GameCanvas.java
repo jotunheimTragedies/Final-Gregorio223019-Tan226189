3cc2495b933e904fd3f2dd572cfcf454cd577004
jotunheimTragedies <patricia.Tan2908@gmail.com>
2023-04-18 00:51:32 +0800

    made the tentative title screen

--- GameCanvas.java
+++ GameCanvas.java
@@ -1,29 +1,84 @@
 
+//import java.awt.image.BufferedImage;
+//import java.io.IOException;
 import java.awt.*;
-import java.awt.geom.Rectangle2D;
 
+//import javax.imageio.ImageIO;
+//import javax.imageio.ImageIO;
 import javax.swing.*;
+import java.awt.geom.*;
+import java.io.*;
 
 public class GameCanvas extends JComponent{
     private int width; 
     private int height; 
    
+    public int gameState;
+    public final int titleState = 0; 
+    public final int runningState = 1; 
+
+
+    
+    //private BufferedImage newGameButton;
+    //private BufferedImage howPlayButton; 
+
+
     public GameCanvas(int w, int h) {
         width = w; 
         height = h; 
         setPreferredSize(new Dimension(width, height));
+        gameState = titleState;
+
+      
+
+
+       
+
+       /*  try {
+            newGameButton = ImageIO.read(getClass().getResourceAsStream("ImageIcons/NF New Game Button.png"));
+            howPlayButton = ImageIO.read(getClass().getResourceAsStream("ImageIcons/NF How Play Button.png"));
+        } catch(IOException e) {
+            e.printStackTrace();
+        } */
     }
 
     @Override
     protected void paintComponent(Graphics g) {
-        Graphics2D g2d = (Graphics2D) g;
+        Graphics2D g2d = (Graphics2D)g;
 
         RenderingHints rh = new RenderingHints(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
         g2d.setRenderingHints(rh);
 
-        // Background Properties
-        Rectangle2D.Double background = new Rectangle2D.Double(0, 0, width, height);
+        Rectangle2D.Double defaultBackground = new Rectangle2D.Double(0, 0, width, height);
         g2d.setPaint(Color.black);
-        g2d.fill(background); 
+        g2d.fill(defaultBackground);
+
+        if(gameState == titleState) {
+            g2d.setFont(new Font("Times New Roman", Font.BOLD, 100));
+            g2d.setPaint(Color.white);
+            g2d.drawString("Remain Speaking and No", 175, 180);
+            g2d.drawString("Human Being Detonates", 190, 300);
+
+            g2d.setFont(new Font("Times New Roman", Font.PLAIN, 55));
+            g2d.drawString("Ateneo CS Edition", 550, 445);
+
+            
+
+           //g2d.drawImage(newGameButton, 350, 550, 300, 100, null);
+           //g2d.drawImage(howPlayButton, 800, 550, 300, 100, null);
+           //(Image img, int x, int y, int width, int height, ImageObserver observer)
+
+
+            // if newGamebutton mouseClicked 
+                // proceed to another game state
+                
+
+        }
+
+        
+    
+       
+
+        
     }
-}
+} 
--- GameFrame.java
+++ GameFrame.java
@@ -1,28 +1,96 @@
 import java.awt.*;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+
 import javax.swing.*;
 
-public class GameFrame {
-    private JFrame gameFrame; 
-    private int width; 
+public class GameFrame implements MouseListener{
+    private int width;
     private int height; 
-    private GameCanvas gameCanvas; 
+    private JFrame gameFrame; 
+    private GameCanvas gameCanvas;
+
+    private JLabel newGameLabel;
+    private JLabel howPlayLabel; 
+    private ImageIcon newGameButton;
+    private ImageIcon howPlayButton; 
 
     public GameFrame(int w, int h) {
         width = w; 
-        height = h;
+        height = h; 
         gameFrame = new JFrame();
         gameCanvas = new GameCanvas(width, height);
+
+        newGameLabel = new JLabel();
+        newGameLabel.addMouseListener(this);
+        howPlayLabel = new JLabel();
+        howPlayLabel.addMouseListener(this);
+
+
+        newGameButton = new ImageIcon("ImageIcons/[NF] New Game Button.png");
+        howPlayButton = new ImageIcon("ImageIcons/[NF] How Play Button.png");
+
+        newGameLabel.setIcon(newGameButton);
+        howPlayLabel.setIcon(howPlayButton);
+        
+
     }
 
     public void setUpGUI() {
         Container cp = gameFrame.getContentPane();
         cp.add(gameCanvas, BorderLayout.CENTER);
 
-        gameFrame.setTitle("[Change Title cause i cant find instructions from specs]");
+        if(gameCanvas.gameState == gameCanvas.titleState) {
+            newGameLabel.setBounds(350, 550, 300, 100);
+            howPlayLabel.setBounds(800, 550, 300, 100);
+            gameFrame.add(newGameLabel);
+            gameFrame.add(howPlayLabel);
+            
+        }
+
+        
+
+        gameFrame.setTitle("Remain Speaking and No Human Being Detonates");
         gameFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         gameFrame.add(gameCanvas);
         gameFrame.pack();
         gameFrame.setVisible(true);
+
+        
+    }
+    
+    // https://stackoverflow.com/questions/35448604/one-mouselistener-for-many-jlabel-components
+    // https://www.youtube.com/watch?v=jptf1Wd_omw
+    // https://www.youtube.com/watch?v=_SJU99LU1IQ - use to continue 
+    @Override 
+    public void mouseClicked(MouseEvent e) {
+        JLabel currentLabel = (JLabel)e.getSource();
+        if(currentLabel == newGameLabel) {
+            System.out.println("Proceed to next gameState");
+        } else if(currentLabel == howPlayLabel) {
+            System.out.println("Proceed to howPlayState");
+        }
+        
     }
 
+    @Override
+    public void mousePressed(MouseEvent e) {
+       
+        //throw new UnsupportedOperationException("Unimplemented method 'mousePressed'");
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent e) {
+        
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent e) {
+       
+    }
+
+    @Override
+    public void mouseExited(MouseEvent e) {
+        
+    }
 }
