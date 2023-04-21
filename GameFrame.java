8e722fc100524a9944d89ed39a0ca2640851f753
Waleed Lugod <lugodwaleed@gmail.com>
2023-04-21 16:02:27 +0800

    timer now shows when running but is fullscreen

--- GameFrame.java
+++ GameFrame.java
@@ -5,22 +5,22 @@ import java.text.*;
 
 public class GameFrame implements MouseListener {
     private int width;
-    private int height; 
-    private JFrame gameFrame; 
+    private int height;
+    private JFrame gameFrame;
     private GameCanvas gameCanvas;
 
     private JLabel newGameLabel;
-    private JLabel howPlayLabel; 
+    private JLabel howPlayLabel;
     private ImageIcon newGameButton;
-    private ImageIcon howPlayButton; 
+    private ImageIcon howPlayButton;
 
-    private BombTimer bombTimer; 
+    private BombTimer bombTimer;
 
-    // can add bombtimer here as an inner class cause it has a thread and a jlabel 
+    // can add bombtimer here as an inner class cause it has a thread and a jlabel
 
     public GameFrame(int w, int h) {
-        width = w; 
-        height = h; 
+        width = w;
+        height = h;
         gameFrame = new JFrame();
         gameCanvas = new GameCanvas(width, height);
 
@@ -29,14 +29,13 @@ public class GameFrame implements MouseListener {
         howPlayLabel = new JLabel();
         howPlayLabel.addMouseListener(this);
 
-
         newGameButton = new ImageIcon("ImageIcons/[NF] New Game Button.png");
         howPlayButton = new ImageIcon("ImageIcons/[NF] How Play Button.png");
 
         newGameLabel.setIcon(newGameButton);
         howPlayLabel.setIcon(howPlayButton);
 
-        //gameCanvas.gameState = gameCanvas.titleState;
+        gameCanvas.gameState = gameCanvas.titleState;
 
     }
 
@@ -44,18 +43,16 @@ public class GameFrame implements MouseListener {
         Container cp = gameFrame.getContentPane();
         cp.add(gameCanvas, BorderLayout.CENTER);
 
-        if(gameCanvas.gameState == gameCanvas.titleState) {
+        if (gameCanvas.gameState == gameCanvas.titleState) {
             System.out.println("setUpGUI");
             newGameLabel.setBounds(350, 550, 300, 100);
             howPlayLabel.setBounds(800, 550, 300, 100);
             gameFrame.add(newGameLabel);
             gameFrame.add(howPlayLabel);
-            
-        } else if(gameCanvas.gameState == gameCanvas.runningState) {
+
+        } else if (gameCanvas.gameState == gameCanvas.runningState) {
             System.out.println("setUpGUI");
-            bombTimer = new BombTimer(); 
-            JPanel bombPanel = bombTimer.getTimerPanel();
-            gameFrame.add(bombPanel, BorderLayout.CENTER);
+            createBombTimer();
         }
 
         gameFrame.setTitle("Remain Speaking and No Human Being Detonates");
@@ -64,31 +61,31 @@ public class GameFrame implements MouseListener {
         gameFrame.pack();
         gameFrame.setVisible(true);
 
-
     }
-    
+
     // https://stackoverflow.com/questions/35448604/one-mouselistener-for-many-jlabel-components
     // https://www.youtube.com/watch?v=jptf1Wd_omw
-    // https://www.youtube.com/watch?v=_SJU99LU1IQ - use to continue 
-    @Override 
+    // https://www.youtube.com/watch?v=_SJU99LU1IQ - use to continue
+    @Override
     public void mouseClicked(MouseEvent e) {
-        JLabel currentLabel = (JLabel)e.getSource();
-        if(currentLabel == newGameLabel) {
+        JLabel currentLabel = (JLabel) e.getSource();
+        if (currentLabel == newGameLabel) {
             System.out.println("Proceed to next gameState");
-            if(gameCanvas.gameState == gameCanvas.titleState) {
-                gameCanvas.gameState = gameCanvas.runningState; 
+            if (gameCanvas.gameState == gameCanvas.titleState) {
+                gameCanvas.gameState = gameCanvas.runningState;
                 System.out.println(gameCanvas.gameState);
                 System.out.println("beep from GameFrame");
+                createBombTimer();
                 gameCanvas.repaint();
                 newGameLabel.setVisible(false);
                 howPlayLabel.setVisible(false);
                 gameFrame.repaint();
-               
+
             }
 
-        } else if(currentLabel == howPlayLabel) {
+        } else if (currentLabel == howPlayLabel) {
             System.out.println("Proceed to howPlayState");
-            if(gameCanvas.gameState == gameCanvas.titleState) {
+            if (gameCanvas.gameState == gameCanvas.titleState) {
                 gameCanvas.gameState = gameCanvas.howPlayState;
                 System.out.println("rizz from GameFrame");
                 gameCanvas.repaint();
@@ -100,21 +97,28 @@ public class GameFrame implements MouseListener {
 
     @Override
     public void mousePressed(MouseEvent e) {
-        //throw new UnsupportedOperationException("Unimplemented method 'mousePressed'");
+        // throw new UnsupportedOperationException("Unimplemented method
+        // 'mousePressed'");
     }
 
     @Override
     public void mouseReleased(MouseEvent e) {
-        
+
     }
 
     @Override
     public void mouseEntered(MouseEvent e) {
-       
+
     }
 
     @Override
     public void mouseExited(MouseEvent e) {
-        
+
+    }
+
+    private void createBombTimer() {
+        bombTimer = new BombTimer();
+        JPanel bombPanel = bombTimer.getTimerPanel();
+        gameFrame.add(bombPanel, BorderLayout.CENTER);
     }
 }
